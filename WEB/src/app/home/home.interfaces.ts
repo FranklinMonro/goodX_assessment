a560bc00d9a50e39c9ef7ec0bb8b2c44939b8174
FranklinMonro <fbg2468@gmail.com>
2024-03-16 05:04:03 +0000

    auth page

---
+++ WEB/src/app/home/home.interfaces.ts
@@ -0,0 +1,13 @@
+interface Auth {
+    apikey?: object[];
+    awsv4?: object[];
+    basic?: object[];
+    bearer?: object[];
+    digest?:  object[]
+    edgegrid?: object[];
+    hawk?: object[];
+    noauth?: object[];
+    oauth1?: object[];
+    oauth2?: object[];
+    ntlm?: object[];
+}
---
+++ WEB/src/enviroments/environment.ts
@@ -0,0 +1,14 @@
+const environmentGX = {
+    production: false,
+    urlBase: 'https://dev_interview.qagoodx.co.za/api/',
+};
+
+const environmentAPI = {
+    production: false,
+    urlBase: 'http://localhost:3000/goodxapi/',
+};
+
+export {
+    environmentGX,
+    environmentAPI,
+}
