df5db4cbdb68d21943e388823c9b1eeb571e1bbb
FranklinMonro <fbg2468@gmail.com>
2024-03-16 07:29:16 +0200

    auth page

--- WEB/src/app/app-routing.module.ts
+++ WEB/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'login', component: AuthComponent },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
 ];
 
--- WEB/src/app/auth/auth.interceptor.ts
+++ WEB/src/app/auth/auth.interceptor.ts
@@ -1,5 +1,32 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { 
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpParams,
+  HttpHeaders
+} from '@angular/common/http';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+import { AuthService } from './auth.service';
+import { exhaustMap, take } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    // return this.authService.user.pipe(
+    //   take(1),
+    //   exhaustMap((user) => {
+    //     if (!user) {
+    //       return next.handle(request);
+    //     }
+    //     const modifiedRequest = request.clone({
+    //       headers: new HttpHeaders().set('X-Authorization', user?.token!),
+    //     });
+    //     return next.handle(modifiedRequest);
+    //   }),
+    // );
+    return next.handle(request);
+  }
+}
