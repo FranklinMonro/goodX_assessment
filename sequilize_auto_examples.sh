a28b3757b283f4950cec7fdb40b9f982209f27f9
FranklinMonro <fbg2468@gmail.com>
2025-03-20 10:50:28 +0000

    sa file

--- sequilize_auto_examples.sh
+++ sequilize_auto_examples.sh
@@ -1,7 +1,7 @@
 # sequelize-auto -h <host> -d <database> -u <user> -x [password] -p [port]  --dialect [dialect] -c [/path/to/config] -o [/path/to/models] -t [tableName] -l ts
 # ./node_modules/.bin/sequelize-auto -h <host> -d <database> -u <user> -x [password] -p [port]  --dialect [dialect] -c [/path/to/config] -o [/path/to/models] -t [tableName] -l ts
 ## All models
-# ./node_modules/.bin/sequelize-auto -h localhost -d mygc_v3 -u postgres -x XXXXXXXXX -p 5433 --dialect postgres -o ./models-development -l ts
+# ./node_modules/.bin/sequelize-auto -h localhost -d mygc_v3 -u postgres -x postgres -p 5432 --dialect postgres -o ./models-development -l ts
 
 ## Single table
-# ./node_modules/.bin/sequelize-auto -h localhost -d mygc_v3 -u postgres -x XXXXXXXXX -p 5433 --dialect postgres -o ./models-development -t config -l ts
+# ./node_modules/.bin/sequelize-auto -h localhost -d mygc_v3 -u postgres -x postgres -p 5432 --dialect postgres -o ./models-development -t config -l ts
